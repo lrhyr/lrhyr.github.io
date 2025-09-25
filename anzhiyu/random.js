a0874923c038dd889677df7242715931f12c7a9f
李睿涵 <liruihan@liruihandeMacBook-Air.local>
2025-09-26 01:11:54 +0800

    Site updated: 2025-09-26 01:11:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/24/OK了第一篇博客/","2025/09/23/hello-world/"];function toRandomPost(){
+var posts=["2025/09/24/OK了第一篇博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
