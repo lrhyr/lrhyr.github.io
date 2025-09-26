97de37d5c5fdafd0bf5b7e9dec199fa2b8adc996
李睿涵 <liruihan@liruihandeMacBook-Air.local>
2025-09-27 01:19:17 +0800

    Site updated: 2025-09-27 01:19:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/24/OK了第一篇博客/"];function toRandomPost(){
+var posts=["2025/09/27/区块链？比特币？这到底是什么东西啊！/","2025/09/24/OK了第一篇博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
